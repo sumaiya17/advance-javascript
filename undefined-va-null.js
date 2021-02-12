edcd41b7dbcd989b2fd0bac2d7b685e902fdc5d8
sumaiya islaM <sumaiyaislam7217@gmail.com>
2021-02-12 21:00:18 +0600

    null vs undefiner cleared

---
+++ undefined-va-null.js
@@ -0,0 +1,16 @@
+// let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// // function add(num1, num2){
+//     console.log(num1, num2);
+// }
+//  const premik = (name:"smart dude" phone:34589);
+
+// console.log(premik.gf);
+// let fun = undefined;
+
+
+
